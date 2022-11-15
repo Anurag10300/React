d5c8519e04954d80d30614eb4c59cc4c14db86e2
PANWAR <anurag.panwar@capgemini.com>
2022-11-15 09:55:57 +0530

    Props upgrade

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 //import Data from './Mock-data.json';
 import './App.css';
+import React from "react"
 import Articles from "./Components/Article";
 
 function App() {
@@ -8,20 +9,15 @@ function App() {
   const Data = require('./Mock-data.json');
   //console.log(Data.Articles);
 
-  
+  const dataArray = Data.Articles.map(function(x){
+    return (<Articles Name = {x.Name} Article={x.Number} Email={x.Email} Content={x.Content} />)
+  })
 
-
-  return (
-    <>
-
-    <Articles Name = "Anurag" Article="12341234" Email="something" Content="somethig somethigng"  />
-    <Articles Name = "Panwar" Article="12341234" Email="something 2222" Content="somethig somethigng222"  />
-    <Articles Name = "bleh" Article="12341234" Email="something 333" Content="somethig somethigng 33"  />
-    
-
-    </>
-    
-  );
+  return dataArray
 }
 
+//<Articles Name = "Anurag" Article="12341234" Email="something" Content="somethig somethigng"  />
+ //   <Articles Name = "Panwar" Article="12341234" Email="something 2222" Content="somethig somethigng222"  />
+ //   <Articles Name = "bleh" Article="12341234" Email="something 333" Content="somethig somethigng 33"  />
+    
 export default App;
